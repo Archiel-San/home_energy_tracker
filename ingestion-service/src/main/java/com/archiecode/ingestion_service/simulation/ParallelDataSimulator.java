0651745a05a407b5f9af68d1e94f7351f3ab412a
archiel <archielcruzdossantos@gmail.com>
2026-08-31 21:53:11 +0200

    feat: teste de thread pool inside of the java app. integrate tests for future projects

--- ingestion-service/src/main/java/com/archiecode/ingestion_service/simulation/ParallelDataSimulator.java
+++ ingestion-service/src/main/java/com/archiecode/ingestion_service/simulation/ParallelDataSimulator.java
@@ -1,12 +1,19 @@
 package com.archiecode.ingestion_service.simulation;
 
+import com.archiecode.ingestion_service.dto.EnergyUsageDto;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,7 +23,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Slf4j
 public class ParallelDataSimulator implements CommandLineRunner {
 
-    private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate = new RestTemplate();
     private final Random random = new Random();
 
     @Value("${simulation.requests-per-interval}")
@@ -25,24 +32,56 @@ public class ParallelDataSimulator implements CommandLineRunner {
     @Value("${simulation.parallel-threads}")
     private int parallelThreads;
 
-    //it is used when u want to test many threads running at the same time
-    private final ExecutorService executorService;
+    @Value("${simulation.endpoint}")
+    private String ingestionEndpoint;
 
-    public ParallelDataSimulator(ExecutorService executorService, RestTemplate restTemplate){
-        this.executorService = Executors.newCachedThreadPool();
-        this.restTemplate = restTemplate;
-    }
+    //it is used when u want to test many threads running at the same time
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     @Override
     public void run(String... args) throws Exception {
         log.info("ParallelDataSimulator Started");
         ((ThreadPoolExecutor)executorService).setCorePoolSize(parallelThreads);
-
     }
 
+    //de 1 em 1 min corre isto
     @Scheduled(fixedRateString = "${simulation.interval-ms}")
     public void sendMockData(){
-        //int batchSize = requestPerInterval/
+
+        //batchSize the amount of data that will be retrieved
+        int batchSize = requestPerInterval/parallelThreads;
+        int remainder = requestPerInterval%parallelThreads;
+
+        for (int i =0; i<parallelThreads; i++){
+            int requestsForThread = batchSize + (i< remainder? 1:0);
+            executorService.submit(
+                    ()->{
+                        for(int j=0; j<requestsForThread; j++){
+                            EnergyUsageDto dto = new EnergyUsageDto(
+                                    random.nextLong(1,6),
+                                    Math.round(random.nextDouble(0.0,2.0)*100.0)/100.0,
+                                    LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()
+                            );
+                            try {
+                                HttpHeaders headers = new HttpHeaders();
+                                headers.setContentType(MediaType.APPLICATION_JSON);
+                                HttpEntity<EnergyUsageDto> request = new HttpEntity<>(dto, headers);
+                                restTemplate.postForEntity(ingestionEndpoint, request, Void.class);
+                                log.info("sent mock data: "+dto);
+                            }catch (Exception e){
+                                log.error("Failed to send data: {}", e.getLocalizedMessage());
+                            }
+
+                        }
+                    }
+            );
+        }
+    }
+
+    @PreDestroy
+    public void shutdown(){
+        executorService.shutdown();
+        log.info("ParallelDataSimulator Shut down");
     }
 
 
