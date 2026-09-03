077e16393a4abd45327504ff7cd4fd35d452fe2c
archiel <archielcruzdossantos@gmail.com>
2026-09-03 23:00:22 +0200

    feat: criação do rest template para chamada do device-service

---
+++ usage-service/src/main/java/com/archielcode/usage_service/client/DeviceClient.java
@@ -0,0 +1,35 @@
+package com.archielcode.usage_service.client;
+
+import com.archielcode.usage_service.dto.DeviceDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class DeviceClient {
+
+    private final RestTemplate restTemplate;
+
+    //@Value("${device.service.url}")
+    private final String baseUrl;
+
+    public DeviceClient (@Value("${device.service.url}")String baseUrl){
+        this.restTemplate = new RestTemplate();
+        this.baseUrl = baseUrl;
+    }
+
+    public DeviceDto getDeviceById(Long deviceId){
+        String url = UriComponentsBuilder
+                .fromUriString(baseUrl)
+                .path("/{deviceId}")
+                .buildAndExpand(deviceId)
+                .toUriString();
+
+        //ResponseEntity<DeviceDto> response = restTemplate.getForEntity(url, DeviceDto.class);
+        return null;//response.getBody();
+
+    }
+
+}
