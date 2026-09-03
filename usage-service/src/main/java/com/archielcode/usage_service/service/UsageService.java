30a106a3aa6a960fb4c32e0bc07ae6863b87ccf9
archiel <archielcruzdossantos@gmail.com>
2026-09-03 19:30:31 +0200

    feat: continuation of the usage service

--- usage-service/src/main/java/com/archielcode/usage_service/service/UsageService.java
+++ usage-service/src/main/java/com/archielcode/usage_service/service/UsageService.java
@@ -1,14 +1,22 @@
 package com.archielcode.usage_service.service;
 
 import com.archielcode.kafka.event.EnergyUsageEvent;
+import com.archielcode.usage_service.model.DeviceEnergy;
 import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.QueryApi;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
+import com.influxdb.query.FluxRecord;
+import com.influxdb.query.FluxTable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -47,8 +55,41 @@ public class UsageService {
 
     @Scheduled(cron = "*10 * * * * *")
     public void aggregateDeviceEnergyUsage(){
+        final Instant now = Instant.now();
+        final Instant oneHourAgo = now.minusSeconds(1000);
 
+        String fluxQuery = String.format("""
+        from(bucket: "%s")
+          |> range(start: time(v: "%s"), stop: time(v: "%s"))
+          |> filter(fn: (r) => r["_measurement"] == "energy_usage")
+          |> filter(fn: (r) => r["_field"] == "energyConsumed")
+          |> group(columns: ["deviceId"])
+          |> sum(column: "_value")
+        """, influxBucket, oneHourAgo.toString(), now);
 
+        QueryApi queryApi = influxDBClient.getQueryApi();
+        List<FluxTable> tables = queryApi.query(fluxQuery, influxOrg);
+
+        List<DeviceEnergy> deviceEnergies = new ArrayList<>();
+
+        for(FluxTable table: tables){
+            for (FluxRecord record : table.getRecords()){
+                String deviceIdStr = (String) record.getValueByKey("deviceId");
+                Double energyConsumed = record.getValueByKey("_value") instanceof Number? ((Number) record.getValueByKey("_value")).doubleValue(): 0.0;
+
+                deviceEnergies.add(
+                        DeviceEnergy.builder()
+                                .deviceId(Long.valueOf(deviceIdStr))
+                                .energyConsumed(energyConsumed)
+                                .build()
+                );
+            }
+        }
+        log.info("Aggregated device energies over the past hour: {}", deviceEnergies);
+
+        for (DeviceEnergy deviceEnergy: deviceEnergies){
+            //final DeviceDto deviceResponse = deviceClient
+        }
 
     }
 
