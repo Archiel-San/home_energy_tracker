580ed9df8b30460ad45785682b26d6393c6f53e5
archiel <archielcruzdossantos@gmail.com>
2026-09-08 09:54:50 +0200

    feat: classe serviço de envio de e-mails

---
+++ alert-service/src/main/java/com/archiecode/alert_service/service/EmailService.java
@@ -0,0 +1,57 @@
+package com.archiecode.alert_service.service;
+
+import com.archiecode.alert_service.entity.Alert;
+import com.archiecode.alert_service.repository.AlertRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final AlertRepository alertRepository;
+
+    public EmailService(JavaMailSender javaMailSender, AlertRepository alertRepository){
+        this.mailSender= javaMailSender;
+        this.alertRepository = alertRepository;
+    }
+
+    public void sendEmail (String to, String subject, String body, Long userId){
+        log.info("Sending email to: {}, subject: {}", to, subject);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setFrom("noreply@leetjourney.com");
+        message.setSubject(subject);
+        message.setText(body);
+
+        try {
+            mailSender.send(message);
+            final Alert alertSent = Alert.builder()
+                    .sent(true)
+                    .createdAt(LocalDateTime.now())
+                    .userId(userId)
+                    .build();
+
+            alertRepository.saveAndFlush(alertSent);
+        }
+        catch (MailException e){
+            log.error("Failed to send email to: {}", to, e);
+
+            final Alert alertSent = Alert.builder()
+                    .sent(false)
+                    .createdAt(LocalDateTime.now())
+                    .userId(userId)
+                    .build();
+        }
+        log.info("Email sent to: {}", to);
+
+
+    }
+
+}
