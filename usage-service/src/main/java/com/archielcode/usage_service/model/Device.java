0b47e2c9e5220bdc09e9c1a74736fe7600a065e6
archiel <archielcruzdossantos@gmail.com>
2026-09-11 18:58:59 +0100

    feat: modelo para mapear todos os ids dos dispositivos para verificar a existencia do record na base de dados

---
+++ usage-service/src/main/java/com/archielcode/usage_service/model/Device.java
@@ -0,0 +1,21 @@
+package com.archielcode.usage_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Device {
+
+    Long id;
+    String name;
+    String type;
+    String location;
+    Long userId;
+    Double energyConsumed;
+
+}
