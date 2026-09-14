2e07dffcee294b5c006f6cc337ba52d20eafbc6b
archiel <archielcruzdossantos@gmail.com>
2026-09-14 13:51:41 +0100

    feat: fallback route for the user-service (circuit breaker)

--- api-gateway/src/main/java/com/archiecode/api_gateway/route/UserServiceRoutes.java
+++ api-gateway/src/main/java/com/archiecode/api_gateway/route/UserServiceRoutes.java
@@ -1,10 +1,14 @@
 package com.archiecode.api_gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -18,6 +22,19 @@ public class UserServiceRoutes {
         return route("user-service")
                 .route(RequestPredicates.path("/api/v1/user/**"), http())
                 .before(uri("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "userServiceCircuitBreaker",
+                        URI.create("forward:/userFallBackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userFallbackRoute(){
+        return route("userfallbackroute")
+                .route(RequestPredicates.path("/userFallBackRoute"),
+        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("User Service is down"))
                 .build();
     }
 }
